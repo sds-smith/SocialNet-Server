7466b862a600d8b55ee933b6b366dd8311642eaf
Shawn Smith <sds.smith24@gmail.com>
2024-02-03 17:19:38 -0500

    add acao header

--- src/app.js
+++ src/app.js
@@ -13,6 +13,15 @@ app.use(cors({
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
+app.use((_req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", ['http://localhost:3000', 'https://cup-coffee-social.netlify.app/']);
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
+
 app.post('/login', handleLogin);
 
 app.use('/v1', authMiddleware, apolloMiddleware);
