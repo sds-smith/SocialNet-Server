36bc1385e6eab3dc8146bc71dd34d1beb71a5abe
Shawn Smith <sds.smith24@gmail.com>
2024-02-03 12:22:35 -0500

    delete all mock auth and mock data

--- src/controllers/auth.controller.js
+++
@@ -1,6 +0,0 @@
-import { getUser } from "../models/auth.model.js";
-import { getUser as mongoGetUser } from "../models/mongo/index.js"
-
-export const httpGetUser = async (username) => await getUser(username);
-
-export const httpGetGoogleUser = async (user) => await mongoGetUser(user);
