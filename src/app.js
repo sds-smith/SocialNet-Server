6b40622fd027ebe00bb935edb9b0d1283a499d82
Shawn Smith <sds.smith24@gmail.com>
2024-02-03 17:31:32 -0500

    headers

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ app.use(cors({
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
-app.use((_req, res, next) => {
+app.use('/', (_req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", 'https://cup-coffee-social.netlify.app/');
     res.header(
       "Access-Control-Allow-Headers",
